209af50aa94e29639174cedef0c79869a7ac7436
JamesChenevert <shadowentertainmentproductions@gmail.com>
2025-10-08 07:25:54 -0400

    Update!

--- Main.java
+++ Main.java
@@ -1,17 +1,32 @@
-// import scanner
+// import stuff
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // create a scanner
         Scanner game = new Scanner(System.in);
-        try {
-            // to sleep 3 seconds
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            // recommended because catching InterruptedException clears interrupt flag
-            Thread.currentThread().interrupt();
-            // you probably want to quit if the thread is interrupted
+        // go to sleep for any amount of seconds
+        Thread.sleep(500);
+        System.out.println("Hello player, welcome to Survive the Night.");
+        Thread.sleep(1000);
+        System.out.println("In this game you will be playing as a man named Issac Jeffries.");
+        Thread.sleep(1000);
+        System.out.println("Now, would you like to start the game?");
+        System.out.println("y/n: ");
+        String gameStart = game.nextLine();
+
+        // make an if statement for choices
+        // if choice is no or not a choice then return and end the program
+        // else if the choice is yes, continue forward
+        if(gameStart.equalsIgnoreCase("n") || gameStart.equalsIgnoreCase("no")) {
+            System.out.println("You haven't even played the game yet.");
+            Thread.sleep(1000);
+            System.out.println("Oh well, goodbye I guess.");
+            return;
+        } else if(gameStart.equalsIgnoreCase("y") || gameStart.equalsIgnoreCase("yes")) {
+
+        } else {
+            System.out.println("You didn't even put a valid option.");
             return;
         }
     }
